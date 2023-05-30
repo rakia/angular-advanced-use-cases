c2565c786c43b4d0c744912c751542d97a1c6238
Rakia Ben Sassi <rakia.bensassi@webenius.com>
2023-05-30 10:53:21 +0200

    Fix error and disabled states handling + trigger click on input-fiel-upload when onContainerClick is triggered + support multiple files upload

--- projects/file-upload/src/lib/file-upload.component.ts
+++ projects/file-upload/src/lib/file-upload.component.ts
@@ -2,17 +2,17 @@ import { ControlValueAccessor, NgControl, ReactiveFormsModule, UntypedFormContro
 import {
   ChangeDetectionStrategy,
   Component,
-  EventEmitter,
+  ElementRef,
   HostBinding,
   Input,
   OnDestroy,
-  Output,
-  Self
+  Self,
+  ViewChild,
 } from '@angular/core';
 import { MatFormFieldControl, MatFormFieldModule } from '@angular/material/form-field';
 import { coerceBooleanProperty } from '@angular/cdk/coercion';
 import { CommonModule } from '@angular/common';
-import { Subject } from 'rxjs';
+import { filter, Subject, takeUntil } from 'rxjs';
 
 let nextId = 0;
 
@@ -24,9 +24,10 @@ let nextId = 0;
   imports: [CommonModule, MatFormFieldModule, ReactiveFormsModule],
   providers: [{ provide: MatFormFieldControl, useExisting: FileUploadComponent }],
 })
-export class FileUploadComponent implements ControlValueAccessor, MatFormFieldControl<File>, OnDestroy {
+export class FileUploadComponent implements ControlValueAccessor, MatFormFieldControl<File[]>, OnDestroy {
   @Input() accept: string = 'text/yaml,.yaml,.yml';
-  @Output() uploadFile = new EventEmitter<File>();
+
+  @ViewChild('input', { read: ElementRef }) inputFileUpload: ElementRef | undefined;
   fileControl = new UntypedFormControl(undefined, Validators.required);
   fileName = '';
 
@@ -38,10 +39,12 @@ export class FileUploadComponent implements ControlValueAccessor, MatFormFieldCo
   // eslint-disable-next-line no-plusplus
   id = `lib-file-upload-${nextId++}`;
   describedBy = '';
+  // eslint-disable-next-line no-underscore-dangle
+  private _multiple: boolean = false;
 
   @HostBinding('attr.aria-describedby') ariaDescribedby: string | undefined;
   // @ts-ignore
-  _value: File | null;
+  _value: File[] | null;
 
   get empty() {
     return !this.fileControl.value;
@@ -49,6 +52,17 @@ export class FileUploadComponent implements ControlValueAccessor, MatFormFieldCo
 
   get shouldLabelFloat() {
     return this.focused || !this.empty;
+  }
+
+  @Input()
+  get multiple(): boolean {
+    // eslint-disable-next-line no-underscore-dangle
+    return this._multiple;
+  }
+  set multiple(value: boolean | string) {
+    // eslint-disable-next-line no-underscore-dangle
+    this._multiple = coerceBooleanProperty(value);
+    this.stateChanges.next();
   }
 
   @Input()
@@ -69,12 +83,17 @@ export class FileUploadComponent implements ControlValueAccessor, MatFormFieldCo
     return this._required;
   }
   set required(value: boolean) {
+    if (value) {
+      this.fileControl.setValidators(Validators.required);
+    }
     // eslint-disable-next-line no-underscore-dangle
     this._required = coerceBooleanProperty(value);
     this.stateChanges.next();
   }
   // eslint-disable-next-line no-underscore-dangle
   private _required = false;
+  private _disabled = false;
+  private destroy$ = new Subject<void>();
 
   @Input()
   get disabled(): boolean {
@@ -82,38 +101,57 @@ export class FileUploadComponent implements ControlValueAccessor, MatFormFieldCo
     return this._disabled;
   }
   set disabled(value: boolean) {
+    if (value) {
+      this.fileControl.disable();
+    } else {
+      this.fileControl.enable();
+    }
     // eslint-disable-next-line no-underscore-dangle
     this._disabled = coerceBooleanProperty(value);
     this.stateChanges.next();
   }
-  private _disabled = false;
 
   @Input()
-  get value(): File | null {
+  get value(): File[] | null {
     return this.fileControl.value;
   }
-  set value(file: File | null) {
-    this.fileControl.setValue(file);
+  set value(files: File[] | null) {
+    this.fileControl.setValue(files);
     this.stateChanges.next();
   }
 
   constructor(@Self() public ngControl: NgControl) {
     // eslint-disable-next-line no-param-reassign
     ngControl.valueAccessor = this;
-    this.fileControl.statusChanges.subscribe((status) => {
-      this.errorState = status === 'INVALID';
-      this.stateChanges.next();
-    });
+    this.fileControl.statusChanges
+      .pipe(
+        takeUntil(this.destroy$),
+        filter(() => this.fileControl.dirty || this.fileControl.touched)
+      )
+      .subscribe((status) => {
+        this.errorState = status === 'INVALID';
+        this.stateChanges.next();
+      });
   }
 
   setDescribedByIds(ids: string[]) {
     this.describedBy = ids.join(' ');
   }
 
-  onContainerClick() {} // event: MouseEvent
+  onContainerClick(event: MouseEvent) {
+    if (!this.disabled && (event.target as Element).tagName.toLowerCase() !== 'input') {
+      this.inputFileUpload?.nativeElement.focus();
+      this.inputFileUpload?.nativeElement.click();
+      this.focused = true;
+      this.fileControl.markAsTouched();
+      this.fileControl.updateValueAndValidity();
+      this.stateChanges.next();
+    }
+  }
 
-  writeValue(file: File): void {
-    this.value = file;
+  writeValue(files: File[]): void {
+    this.value = files;
+    this.fileControl.setValue(files);
   }
 
   registerOnChange(fn: any): void {
@@ -131,17 +169,20 @@ export class FileUploadComponent implements ControlValueAccessor, MatFormFieldCo
   }
 
   onFileSelected(event: any) {
-    // Event
-    const file: File = event.target.files[0]; // (event.target as HTMLInputElement)?.files[0]
-    if (file) {
-      this.fileName = file.name;
-      this.fileControl.setValue(file);
+    // eslint-disable-next-line prefer-destructuring
+    const files: File[] = event.target.files; // (event.target as HTMLInputElement)?.files
+    if (files) {
+      this.fileName = files.length > 1 ? `${files.length} files to upload` : files[0].name;
+      this.fileControl.setValue(files);
+      this.value = files;
       this.onTouched();
-      this.uploadFile.emit(file);
+      this.stateChanges.next();
     }
   }
 
   ngOnDestroy() {
     this.stateChanges.complete();
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
