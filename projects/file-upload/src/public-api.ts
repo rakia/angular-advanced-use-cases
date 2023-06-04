70bc1f5cbb64f2ecb9c84de17b630f3a05bed761
Rakia Ben Sassi <rakia.bensassi@webenius.com>
2023-06-04 17:26:50 +0200

    clean up project structure

--- projects/file-upload/src/public-api.ts
+++ projects/file-upload/src/public-api.ts
@@ -2,5 +2,5 @@
  * Public API Surface of file-upload
  */
 
-export * from './lib/file-upload.component';
-export * from './lib/file-upload.module';
+export * from './lib/file-upload/components/file-upload.component';
+export * from './lib/file-upload/file-upload.module';
