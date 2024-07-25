d40e001be26b7c0d177cd7075721f6b16097ad7a
Rakia Ben Sassi <rakia.bensassi@webenius.com>
2024-07-25 22:24:08 +0200

    Add jsDoc to ngOnInit() in EcsFieldsetsContainerComponent

--- src/app/modules/ecs-fieldsets/containers/ecs-fieldsets-container.component.ts
+++ src/app/modules/ecs-fieldsets/containers/ecs-fieldsets-container.component.ts
@@ -33,6 +33,9 @@ export class EcsFieldsetsContainerComponent implements OnInit, OnDestroy {
 
   constructor(private storeService: EcsFieldsetsStoreService, private activatedRoute: ActivatedRoute) {}
 
+  /**
+   * In this lifecycle hook we retrieve data from the API that is needed for this component
+   */
   async ngOnInit(): Promise<void> {
     this.releaseId = this.activatedRoute.parent?.snapshot?.paramMap.get('releaseId');
     this.storeService.getRelease(this.releaseId!);
